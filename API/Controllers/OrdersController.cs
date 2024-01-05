4f58b12d0064779ee5adf900a50fae98596dfa99
Raul Ratiu <raulratiu2@gmail.com>
2024-01-05 10:23:31 +0200

    fix: fix typo in OrdersController

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -45,7 +45,7 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<int>> CreateOrder(CreateOrderDto orderDto)
+        public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
         {
             var basket = await _context
                 .Baskets
@@ -74,46 +74,47 @@ namespace API.Controllers
                 };
                 items.Add(orderItem);
                 productItem.QuantityInStock -= item.Quantity;
-
-                var subtotal = items.Sum(item => item.Price * item.Quantity);
-                var deliveryFee = subtotal > 1000 ? 0 : 500;
-
-                var order = new Order
-                {
-                    OrderItems = items,
-                    BuyerId = User.Identity.Name,
-                    ShippingAddress = orderDto.ShippingAddress,
-                    Subtotal = subtotal,
-                    DeliveryFee = deliveryFee
-                };
-
-                _context.Orders.Add(order);
-                _context.Baskets.Remove(basket);
-
-                if (orderDto.SaveAddress)
-                {
-                    var user = await _context
-                        .Users
-                        .FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
-                    user.Address = new UserAddress
-                    {
-                        FullName = orderDto.ShippingAddress.FullName,
-                        Address1 = orderDto.ShippingAddress.Address1,
-                        Address2 = orderDto.ShippingAddress.Address2,
-                        City = orderDto.ShippingAddress.City,
-                        State = orderDto.ShippingAddress.State,
-                        Zip = orderDto.ShippingAddress.Zip,
-                        Country = orderDto.ShippingAddress.Country
-                    };
-
-                    var result = await _context.SaveChangesAsync() > 0;
-
-                    if (result)
-                        return CreatedAtRoute("GetOrder", new { id = order.Id }, order.Id);
-
-                    return BadRequest("Problem creating order");
-                }
             }
+
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+            var deliveryFee = subtotal > 1000 ? 0 : 500;
+
+            var order = new Order
+            {
+                OrderItems = items,
+                BuyerId = User.Identity.Name,
+                ShippingAddress = orderDto.ShippingAddress,
+                Subtotal = subtotal,
+                DeliveryFee = deliveryFee
+            };
+
+            _context.Orders.Add(order);
+            _context.Baskets.Remove(basket);
+
+            if (orderDto.SaveAddress)
+            {
+                var user = await _context
+                    .Users
+                    .FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+                user.Address = new UserAddress
+                {
+                    FullName = orderDto.ShippingAddress.FullName,
+                    Address1 = orderDto.ShippingAddress.Address1,
+                    Address2 = orderDto.ShippingAddress.Address2,
+                    City = orderDto.ShippingAddress.City,
+                    State = orderDto.ShippingAddress.State,
+                    Zip = orderDto.ShippingAddress.Zip,
+                    Country = orderDto.ShippingAddress.Country
+                };
+                _context.Update(user);
+            }
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result)
+                return CreatedAtRoute("GetOrder", new { id = order.Id }, order.Id);
+
+            return BadRequest("Problem creating order");
         }
     }
 }
